bb5fa372ca23a9b1ea16ed611d6b8c80ed85396a
johan <johan@bartox.nl>
2015-06-22 16:54:24 +0200

    Initial checkin

---
+++ ADCTouchClickButton.h
@@ -0,0 +1,45 @@
+#ifndef ADCTouchClickButton_H
+#define ADCTouchClickButton_H
+
+#if (ARDUINO <  100)
+#include <WProgram.h>
+#else
+#include <Arduino.h>
+#endif
+
+#define CLICKBTN_PULLUP HIGH
+
+#ifndef ADCT_h
+#define ADCT_h
+#endif
+
+class ADCTouchClickButton
+{
+  public:
+    ADCTouchClickButton(uint8_t buttonPin, int adcSamples, int adcLimit);
+    ADCTouchClickButton(uint8_t buttonPin, int adcSamples, int adcLimit, boolean active);
+    ADCTouchClickButton(uint8_t buttonPin, int adcSamples, int adcLimit, boolean active, boolean internalPullup);
+    void Update();
+	void createReference(int adcSamples);
+    int clicks;                   // button click counts to return
+    boolean depressed;            // the currently debounced button (press) state (presumably it is not sad :)
+    long debounceTime;
+    long multiclickTime;
+    long longClickTime;
+
+	int limit;	
+	int samples;
+//	int ADCChannel;
+	int touchRef; 
+  private:
+    uint8_t _pin;                 // Arduino pin connected to the button
+    boolean _activeHigh;          // Type of button: Active-low = 0 or active-high = 1
+    boolean _btnState;            // Current appearant button state
+    boolean _lastState;           // previous button reading
+    int _clickCount;              // Number of button clicks within multiclickTime milliseconds
+    long _lastBounceTime;         // the last time the button input pin was toggled, due to noise or a press
+	int read();
+};
+
+
+#endif
---
+++ examples/ADCTouchClickButton.ino
@@ -0,0 +1,35 @@
+#include <ADCTouchClickButton.h>
+
+ADCTouchClickButton button1(A0, 10, 250, LOW, CLICKBTN_PULLUP);
+    
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Start");
+  button1.debounceTime = 20;   // Debounce timer in ms
+  button1.multiclickTime = 250;  // Time limit for multi clicks
+  button1.longClickTime = 1000; // time until "held-down clicks" register
+  button1.createReference(100);
+}
+
+void loop() {
+  button1.Update();
+  if (button1.clicks == 1) {
+     Serial.println("Single");
+  }
+  if (button1.clicks == 2) {
+     Serial.println("Double");
+  }
+   if (button1.clicks == 3) {
+     Serial.println("3 Double");
+  }  
+  if (button1.clicks == 4) {
+     Serial.println("4 Double");
+  } 
+  if (button1.clicks == -3) {
+     Serial.println("3 long");
+  }  
+  if (button1.clicks == -1) {
+    Serial.println("long");
+  }  
+  delay(50);
+}
